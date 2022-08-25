6a54bbf7133b4d3b516fd3ad40e26533a65c6d23
ZhangYahan <yahan233@163.com>
2022-08-25 14:08:52 +0800

    初始化脚手架.

---
+++ src/main.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './App'
+import './index.css'
+
+const root = document.getElementById('root')
+createRoot(root)
+  .render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+  )
