4e2f78ba5a4ad50989abb70867e5e0a83f6dfc6d
ZhangYahan <yahan233@163.com,zyahan1997@163.com>
2023-08-13 09:42:02 +0800

    fix: 新增 eslintrc 规则 curly 的自定义.

--- src/main.tsx
+++ src/main.tsx
@@ -7,9 +7,8 @@ import { createRoot } from 'react-dom/client'
 import App from './App'
 
 const root = document.getElementById('root')
-createRoot(root!)
-  .render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>,
-  )
+createRoot(root!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
